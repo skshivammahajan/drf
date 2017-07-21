25f7f6eb53efaf62054ac2d7f220ef64b6d19c92
skshivammahajan <skshivammahajan@gmail.com>
2017-07-21 13:07:23 +0530

    Hyperlinked APIs

--- DRF/tutorial/tutorial/tutorial/urls.py
+++ DRF/tutorial/tutorial/tutorial/urls.py
@@ -1,15 +1,43 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from quickstart import views
+# from quickstart import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^', include('snippets.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# urlpatterns = [
+#     url(r'^', include(router.urls)),
+#     url(r'^', include('snippets.urls')),
+#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
+
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    url(r'^$', views.api_root),
+    url(r'^snippets/$',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail')
+])
+
+# Login and logout views for the browsable API
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
